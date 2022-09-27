af72112794f737d3252854107cc54717f00eab76
jackaihfia2334 <2598207826@qq.com>
2022-09-27 16:53:55 +0800

    同步

--- python/py_study.py
+++ python/py_study.py
@@ -38,3 +38,33 @@ x = cat(6,"red")
 #x.__voice()   无法访问
 x._cat__voice()
 
+
+"""
+
+函数sorted(iterable, cmp=None, key=None, reverse=False)
+
+args:
+iterable -- 可迭代对象。
+cmp -- 比较的函数，这个具有两个参数，参数的值都是从可迭代对象中取出，此函数必须遵守的规则为，大于则返回1，小于则返回-1，等于则返回0。
+key -- 主要是用来进行比较的元素，只有一个参数，具体的函数的参数就是取自于可迭代对象中，指定可迭代对象中的一个元素来进行排序。
+reverse -- 排序规则，reverse = True 降序 ， reverse = False 升序（默认）。
+
+return：
+排序后的对象
+
+"""
+
+a = [5,7,6,3,4,1,2]
+b = sorted(a)       # 保留原列表
+print(a)
+print(b)
+ 
+L=[('b',2),('a',1),('c',3),('d',4)]
+sorted(L, cmp=lambda x,y:cmp(x[1],y[1]))   # 利用cmp函数
+sorted(L, key=lambda x:x[1])               # 利用key
+
+ 
+ 
+students = [('john', 'A', 15), ('jane', 'B', 12), ('dave', 'B', 10)]
+sorted(students, key=lambda s: s[2])            # 按年龄排序
+sorted(students, key=lambda s: s[2], reverse=True)       # 按降序
