f336b4edaa1f580bdb367bbcf7bd677a22e36ce2
jackaihfia2334 <2598207826@qq.com>
2022-12-01 15:56:01 +0800

    12.1

--- PyTorch/deep-learning-for-image-processing/pytorch_classification/analyze_weights_featuremap/analyze_feature_map.py
+++ PyTorch/deep-learning-for-image-processing/pytorch_classification/analyze_weights_featuremap/analyze_feature_map.py
@@ -6,27 +6,27 @@ import numpy as np
 from PIL import Image
 from torchvision import transforms
 
-data_transform = transforms.Compose(
-    [transforms.Resize((224, 224)),
-     transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+#data_transform = transforms.Compose(
+#    [transforms.Resize((224, 224)),
+#    transforms.ToTensor(),
+#   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-# data_transform = transforms.Compose(
-#     [transforms.Resize(256),
-#      transforms.CenterCrop(224),
-#      transforms.ToTensor(),
-#      transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+data_transform = transforms.Compose(
+     [transforms.Resize(256),
+      transforms.CenterCrop(224),
+      transforms.ToTensor(),
+      transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 
 # create model
-model = AlexNet(num_classes=5)
-# model = resnet34(num_classes=5)
+#model = AlexNet(num_classes=5) 
+model = resnet34(num_classes=1000)
 # load model weights
-model_weight_path = "./AlexNet.pth"  # "./resNet34.pth"
+model_weight_path = "./resnet34.pth"  # "./AlexNet.pth"
 model.load_state_dict(torch.load(model_weight_path))
 print(model)
 
 # load image
-img = Image.open("../tulip.jpg")
+img = Image.open("./tulip.jpg")
 # [N, C, H, W]
 img = data_transform(img)
 # expand batch dimension
